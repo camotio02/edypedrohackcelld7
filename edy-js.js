feaa50c1c1f8827a11946e36589eef01196294f6
camotio02 <105454410+camotio02@users.noreply.github.com>
2022-09-25 22:04:54 -0300

    Add files via upload

---
+++ edy-js.js
@@ -0,0 +1,10 @@
+var links = document.querySelector('div.links');
+const showMenu = () => {
+    links.style.display = 'flex';
+}
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700
+  }).reveal(`
+   `)
