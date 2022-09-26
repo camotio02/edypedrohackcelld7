84cc527859fff42a823a8b14705e33d9ad92bd2d
camotio02 <105454410+camotio02@users.noreply.github.com>
2022-09-26 20:28:06 -0300

    estou mexendo no meu projeto em geral

--- edy-js.js
+++ edy-js.js
@@ -2,11 +2,9 @@ var links = document.querySelector('div.links');
 var closemenu = document.querySelector('div.closemenu')
 const showMenu = () => {
     links.style.display = 'flex';
-    closemenu.style.display = 'none';
 }
 const closemenu = () => {
     links.style.display = 'none';
-    closemenu.style.display = 'none';
 }
 ScrollReveal({
     origin: 'top',
