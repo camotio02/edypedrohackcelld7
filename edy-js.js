71598f189e4e2337f45df95f54c9b7d1ffd6e891
camotio02 <105454410+camotio02@users.noreply.github.com>
2022-09-26 22:05:08 -0300

    js

--- edy-js.js
+++ edy-js.js
@@ -5,6 +5,15 @@ const showMenu = () => {
 const closemenu = () => {
     links.classList.remove("shows");
 }
+const showAdminItens = () => {
+    const admin = window.prompt('digete a sua senha de administrador por favor!').value
+    if (!admin) {
+        alert('você não digitou nada, por favor insira sua senha')
+    } else if (admin == 'camotio') {
+        var senha = window.prompt('digete a sua senha de administrador por favor!').value
+    } else {
+        return
+    }
 ScrollReveal({
     origin: 'top',
     distance: '30px',
